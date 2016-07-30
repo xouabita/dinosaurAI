5c27fa5ff9d3c4327b3948449122d1ba6bf6b5c7
Alexandre Abita <abita_a@epita.fr>
2016-07-30 10:47:26 +0200

    Show infos

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-// Constants
+// dinosaurAI
 // =========
 //
-// Some constants helpers
+// Help a T-Rex to jump cactus thanks to neural network and genetic algorithms
 
-const instance  = Runner.instance_
-const obstacles = Runner.instance_.horizon.obstacles
-const {tRex}    = instance
+// Constants
+// ---------
+
+// Some constants helpers
+const instance        = Runner.instance_
+const {tRex, horizon} = instance
+const REFRESH         = 50
+
+
+
 
 // Game informations
-// =================
+// -----------------
 //
 // In this part of the code, we get all the informations about the game.
 
-// Getters
-// -------
-
 // Get the next obstacle. Return undefined if there is no next obstacle.
-function getNextObstacle() {
+window.getNextObstacle = function getNextObstacle() {
+
+  const {obstacles} = horizon
+
   if (!obstacles.length)
     return undefined
 
@@ -38,6 +45,7 @@ function getDistance() {
 // Get the height of the next obstacle
 function getHeight() {
   let obstacle = getNextObstacle()
+
   if (!obstacle)
     return 0
   else
@@ -55,6 +63,58 @@ let distance = getDistance()
 let height   = getHeight()
 
 // We constantly update informations about the game
-const updateSpeed    = setInterval(() => speed    = getSpeed(), 200)
-const updateDistance = setInterval(() => distance = getDistance(), 200)
-const updateHeight   = setInterval(() => height   = getHeight(), 200)
+const updateSpeed    = setInterval(() => speed    = getSpeed(), REFRESH)
+const updateDistance = setInterval(() => distance = getDistance(), REFRESH)
+const updateHeight   = setInterval(() => height   = getHeight(), REFRESH)
+
+
+
+
+// UI
+// --
+//
+// Display all the informations we currently got about the game.
+
+// Helper function to create div
+const elm = () => document.createElement('div')
+
+// Global object which represent the UI
+const UI = {
+  speed: elm(),
+  distance: elm(),
+  height: elm()
+}
+
+// Patch the game to display the UI
+function patchGame() {
+  const game = elm()
+  const menu = elm()
+
+  game.id = "game"
+  menu.id = "menu"
+
+  document.body.appendChild(game)
+  document.body.appendChild(menu)
+
+  game.appendChild(document.getElementById('main-frame-error'))
+  game.style.display = menu.style.display = 'inline-block'
+
+  game.style.width = '66%'
+  menu.style.width = '33%'
+  menu.style.verticalAlign = 'top'
+
+  for (let k in UI)
+    menu.appendChild(UI[k])
+}
+
+// Function which update the UI with the current infos
+function updateUI() {
+  const setItem = (elt, val) => UI[elt].innerHTML = val
+
+  setItem('speed', `Speed: ${speed}`)
+  setItem('distance', `Distance: ${distance}`)
+  setItem('height', `Height: ${height}`)
+}
+
+patchGame()
+const constantlyUpdateUI = setInterval(updateUI, REFRESH)
